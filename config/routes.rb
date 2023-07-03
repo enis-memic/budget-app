b1c0b1c943923ba6ff04900ad8fc60f69d879c44
Enis <enis.mema@gmail.com>
2023-07-03 12:45:46 +0200

    Set up models

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,9 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root to: 'home#index'
+
+  resources :users
+  resources :categories do 
+    resources :transaction 
+  end
 end
